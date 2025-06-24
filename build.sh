02e0e53f21729c6808d374183b52e5fb64570ae9
Arnav12435 <arnavmohantyy1@gmail.com>
2025-06-24 12:56:04 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit  # Exit on error
-
-# Update the package list
-apt-get update
-
-# Install system packages required by xmlsec
-apt-get install -y libxml2-dev libxmlsec1-dev pkg-config xmlsec1 python3-dev
